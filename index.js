4a8c0c3b6dfc8e4d1113d4bb4020eb2a2913e1e2
A MacGregor <annabel.macgregor@gmail.com>
2018-03-12 21:36:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,11 @@ function sayHiToGrandma(string) {
   var lowercase = `${string}`.toLowerCase
   if(`${string}` === "I love you, Grandma.") {
    return "I love you, too." 
+  } else if(`${string}` === uppercase) {
+    return "YES INDEED!"
+  } else if(`` === lowercase) {
+    return ""
+  }
+  }
  } 
 }
