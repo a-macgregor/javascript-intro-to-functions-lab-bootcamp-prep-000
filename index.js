a5f60be7d55706cf1ceeee07a294505371cc3ddc
A MacGregor <annabel.macgregor@gmail.com>
2018-03-12 20:51:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function shout(string) {
   return string.toUpperCase();
 }
+
 function whisper(string) {
   return string.toLowerCase();
 }
+
 function logShout(string) {
-  console.log(Hello.toUpperCase);
+  console.log(string);
 }
+
+logShout.toUpperCase
